8223d9339f919b38c8e1e7538c6a8c1c02fb3d40
MohamedFadelMoumeni <jackbadas01@gmail.com>
2022-02-24 17:59:32 +0100

    up

--- index.js
+++ index.js
@@ -15,6 +15,37 @@ app.get('/', (req, res) => {
     res.json({msg: "Welcome"});
 })
 
+app.get('/pay/:priceProduct/:name', async (req, res) => {
+  
+
+  try{
+    const {priceProduct, name} = req.params;
+    const stripe = require('stripe')('sk_test_51KP75OHR4mumGY8iJTRnia6tVLbBiBy1cCzh5CfdZngEURzvGs6PyYkw7SXLiWk04XdoiDC2kj48Kons7zhw85K000fpGYDOvY');
+    const product = await stripe.products.create({
+      name: name,
+    });
+    const price = await stripe.prices.create({
+      unit_amount: priceProduct*100,
+      currency: 'usd',
+      recurring: {interval: 'month'},
+      product: product.id
+    });
+   
+const paymentLink = await stripe.paymentLinks.create({
+  line_items: [
+    {
+      price: price.id,
+      quantity: 1,
+    },
+  ],
+  after_completion: {type: 'redirect', redirect: {url: 'https://google.com'}},
+});
+res.send({link : paymentLink});
+  }catch(e){
+    res.send({err: e.message});
+  }
+})
+
 app.post('/send', async (req, res) => {
 
     console.log(req.body);
